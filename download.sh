a76d0496e4fe50345a883f610dbab953d71fa9f7
Aerin Kim <ohmyheroine@hotmail.com>
2018-06-26 15:50:37 -0700

    added wget script to download version 2.0 files

--- download.sh
+++ download.sh
@@ -12,6 +12,9 @@ mkdir $DATA_DIR
 wget https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json -O $DATA_DIR/train-v1.1.json
 wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json -O $DATA_DIR/dev-v1.1.json
 
+wget https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json  -O $DATA_DIR/train-v2.0.json
+wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json  -O $DATA_DIR/dev-v2.0.json
+
 
 # Download GloVe
 wget http://nlp.stanford.edu/data/glove.840B.300d.zip -O $DATA_DIR/glove.840B.300d.zip
