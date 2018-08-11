6e67b99e6eaee821e4337aea4a425b24506b24fb
Aerin Kim <ohmyheroine@hotmail.com>
2018-08-11 02:40:04 -0700

    implemented all-but-the-top

--- my_utils/word2vec_utils.py
+++ my_utils/word2vec_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from .tokenizer import normalize_text
+from sklearn.decomposition import PCA
 
 def load_glove_vocab(path, glove_dim, wv_dim=300):
     vocab = set()
@@ -22,4 +23,20 @@ def build_embedding(path, targ_vocab, wv_dim):
             token = normalize_text(' '.join(elems[0:-wv_dim]))
             if token in w2id:
                 emb[w2id[token]] = [float(v) for v in elems[-wv_dim:]]
-    return emb
+
+    v = emb
+    # Implement all-but-the-top
+    mu = np.mean(v, axis=0)
+    v_tilde = v - mu  
+
+    D=3
+    # Compute the PCA components
+    pca = PCA(n_components=D)
+    u = pca.fit_transform(v.T)
+
+    # Postprocess
+    for w in range(v_tilde.shape[0]):
+        for i in range(D):
+            v_tilde[w, :] = v_tilde[w, :] - u[:, i].dot(v[w]) * u[:, i].T
+
+    return v_tilde
